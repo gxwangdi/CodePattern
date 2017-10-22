3614e282028b31d9a39ea09760554893a4aa0364
Di Wang <gxwangdi@gmail.com>
2017-10-22 09:59:37 -0700

    Review Facade Pattern.

--- DesignPatterns/src/com/eclipse/patterns/Facade.java
+++ DesignPatterns/src/com/eclipse/patterns/Facade.java
@@ -1,13 +1,21 @@
 package com.eclipse.patterns;
 
-/**
- * The Facade pattern shields clients from complex subsystem
- * components and provides a simpler programming interface for the general
- * user. However, it does not prevent the advanced user from going to thedeeper,
+/*
+ * Frequently, as your programs evolve and develop, they grow in complexity. 
+ * In fact, for all the excitement about using design patterns, these 
+ * patterns sometimes generate so many classes that it is difficult to 
+ * understand the program’s flow. Furthermore, there may be a number of 
+ * complicated subsystems, each of which has its own complex interface. 
+ * The Façade pattern allows you to simplify this complexity by providing 
+ * a simplified interface to these subsystems. 
+ * 
+ * The Facade pattern shields clients from complex subsystem components 
+ * and provides a simpler programming interface for the general user. 
+ * However, it does not prevent the advanced user from going to the deeper, 
  * more complex classes when necessary.
  * 
- * In addition, the Facade allows you to make changes in the underlying
- * subsystems without requiring changes in the client code, and reduces
+ * In addition, the Facade allows you to make changes in the underlying 
+ * subsystems without requiring changes in the client code, and reduces 
  * compilation dependencies.
 * */
 
@@ -17,7 +25,6 @@ public class Facade {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Like using JDBC to connect to varieties of DB, " +
 				"extract the common part and build an interface, reduce the system dependencies.");
 	}
